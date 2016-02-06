9ec6976d46361a8fb969f65f7258e39c301e41b2
jegtnes <alex.jegtnes@cxpartners.co.uk>
2016-02-06 14:11:06 +0000

    retrieving state and history on the client

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,29 @@
 import React                  from 'react';
 import {render}               from 'react-dom';
 import {Router}               from 'react-router';
-import {createBrowserHistory} from 'history/lib/createBrowserHistory';
+import createBrowserHistory   from 'history/lib/createBrowserHistory';
 import {routes}               from 'routes';
+import {createStore, combineReducers} from 'redux';
+import {Provider}                     from 'react-redux';
+import * as reducers                  from '../shared/reducers';
+import {fromJS}                       from 'immutable';
+
 
 const history = createBrowserHistory();
 
+let initialState = window.__INITIAL_STATE__;
+
+// Transform state into Immutable collections except for top level keys
+Object.keys(initialState).forEach(key => {
+  initialState[key] = fromJS(initialState[key]);
+});
+
+const reducer = combineReducers(reducers);
+const store   = createStore(reducer, initialState);
+
 render(
-  <Router children={routes} history={history} />,
+  <Provider store={store}>
+    <Router children={routes} history={history} />
+  </Provider>,
   document.getElementById('react-view')
 );
